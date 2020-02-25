f7ec1ffb2824f6dc5d519db1db18acbfe41206d5
San Nge <ngesa@plu.edu>
2020-02-25 13:44:49 -0800

    changed controllerTest file

---
+++ ApplicationX/src/test/java/Core/ControllerTest.java
@@ -0,0 +1,12 @@
+package Core;
+import org.junit.*;
+public class ControllerTest {
+    @Test
+    public String getCell(){
+        return null;
+    }
+    @Test
+    public ResearchedCells getAllCell(){
+        return null;
+    }
+}
